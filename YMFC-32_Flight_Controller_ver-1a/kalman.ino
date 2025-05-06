19d1565b9a956b2e4ca37ca13d883ae73d8eb031
gzs12 <168538332+gzs12@users.noreply.github.com>
2025-05-06 16:14:21 +0800

    Update kalman.ino

--- YMFC-32_Flight_Controller_ver-1a/kalman.ino
+++ YMFC-32_Flight_Controller_ver-1a/kalman.ino
@@ -1,16 +1,32 @@
-void kalman_1d(float KalmanState, float KalmanUncertainty, float KalmanInput, float KalmanMeasurement) {
-  KalmanState=KalmanState+0.004*KalmanInput;
+void kalman_1d(float KalmanUncertainty) {  
+  angle_pitch=angle_pitch+0.004*gyro_pitch;
+
+  angle_pitch -= angle_roll * sin((float)gyro_yaw * 0.000001066);                  // 如果IMU有偏航，將滾轉角度轉移到俯仰角度(這是歐拉公式)
+
+  angle_roll=angle_roll+0.004*gyro_roll;
+
+  angle_roll += angle_pitch * sin((float)gyro_yaw * 0.000001066); 
+
+  if (isnan(angle_pitch) || isnan(angle_roll)) { ////////
+    angle_pitch = 0;/////////////////////////
+    angle_roll = 0;/////////////////////
+    }
+
+
   KalmanUncertainty=KalmanUncertainty + 0.004 * 0.004 * 0.22 * 0.005;//0.004*0.004*0.22*0.005
-  float KalmanGain=KalmanUncertainty * 1/(1*KalmanUncertainty + 1* 1);//1*1
-  KalmanState=KalmanState+KalmanGain * (KalmanMeasurement-KalmanState);
+
+
+   KalmanGain=KalmanUncertainty * 1/(1*KalmanUncertainty + 1*1);
+
+  angle_pitch=angle_pitch+KalmanGain * (angle_pitch_acc-angle_pitch);
+
+  angle_roll=angle_roll+KalmanGain * (angle_roll_acc-angle_roll);
+
   KalmanUncertainty=(1-KalmanGain) * KalmanUncertainty;
-  Kalman1DOutput[0]=KalmanState; 
+  
   Kalman1DOutput[1]=KalmanUncertainty;
     //Serial.print("KalmanGain: ");
     //Serial.println(KalmanGain);
-    //delay(20);
-    //Serial.print("Filtered State: ");
-    //Serial.println(*KalmanState);
     //delay(20);
     //Serial.print("KalmanUncertainty: ");
     //Serial.println(*KalmanUncertainty);
